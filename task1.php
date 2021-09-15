6dc39aef65fc1b0d2d07fa5ed49e3cd8e873de03
Shoaib Azhar <shoaib.wpbrigade@gmail.com>
2021-09-15 13:39:55 +0500

    task1 modified

--- task1.php
+++ task1.php
@@ -1,5 +1,21 @@
+<?php declare(strict_types=1);
+?>
+<h1>First Task</h1>
 <?php
-echo("hello world");
-echo("hello world");
+//this is my first comment
+/*
+this is my 
+doublle line comment
+*/
+$myname="Shoaib";
+$fathername= "Azhar";
+$fullname = $myname." " .$fathername;
+echo"hello world <br>";
+echo"My name is " .$myname ." and my father name is $fathername<br>";
+echo $fullname;
+/*
+this also a 
+double line comment
 
+*/
 ?>
