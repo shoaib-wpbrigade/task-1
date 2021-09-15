92c92cf21fa0bd6f9f2a0060df5244ddc1a42407
Shoaib Azhar <shoaib.wpbrigade@gmail.com>
2021-09-15 14:33:06 +0500

    file name changed

--- practice.php
+++ practice.php
@@ -3,6 +3,7 @@ class car{
 public $color;
 public $name;
 public $model;
+//constructor also have default parameters if the value is not given while initializing te object it will take default values
 public function __construct($name="BMW", $model="UK", $color="white")
 {
 	$this->name= $name;
@@ -28,9 +29,15 @@ echo $rizwan->car();
 $shoaib = new car();
 echo $shoaib->car();
 
-$koibhi= new car;
-echo $koibhi->car();
 
+//this is practice for the type casting
+$x=10.954; //float
+$height= (string)$x; //float to string
 
+$height= $height." feet"; //joining two strings
+echo $height;
 
+//constant
+define('myname', 'Shoaib');
+echo "<br> ". myname;
 ?>
