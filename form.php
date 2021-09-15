489652c79e5c0d70d365d02258927e79ca4cb9bb
Shoaib Azhar <shoaib.wpbrigade@gmail.com>
2021-09-15 16:55:02 +0500

    validation done

--- form.php
+++ form.php
@@ -1,25 +1,33 @@
 <html>
-
-<body>
-<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-<label for="name">Name</label>	
-<input type="text" name="name">
-<input type="submit">
-</form>
 <?php
+$nameerr="";
+function validate($data){
+	$data=trim($data);
+	$data=stripslashes($data);
+	$data=htmlspecialchars($data);
+	return $data;
+}
 if($_SERVER['REQUEST_METHOD']=="POST")
 {
-	$name=$_REQUEST['name'];
+	$name=validate($_REQUEST['name']);
 	if(empty($name)){
-		echo "Enter name first";
+		$nameerr= "Enter name first";
 	}
 	else{
+		
 	echo "<h1>Your name is $name</h1>";
 	}
 echo preg_match("/shoaib/", $name);
 }
 
 ?>
+<body>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+<label for="name">Name</label>	<span>* <?php echo $nameerr; ?></span>
+<input type="text" name="name">
+<input type="submit">
+</form>
+
 
 </body>
 
